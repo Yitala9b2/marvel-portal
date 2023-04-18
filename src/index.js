89edafaad6ec652ec76c8e470d89d7da741eb098
Виталий <94127565+Yitala9b2@users.noreply.github.com>
2023-04-18 21:36:18 +0300

    изменен корневой файл

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './style/style.scss';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
-        <App />
+        <App/>
     </React.StrictMode>,
-    document.getElementById('root'),
 );
 
