51eafd728ee134c0a6f3c0b1c523d4dae76bd2e5
Виталий <94127565+Yitala9b2@users.noreply.github.com>
2023-04-19 19:30:14 +0300

    выбор рандомного персонажа

--- src/index.js
+++ src/index.js
@@ -5,13 +5,7 @@ import MarvelService from './services/MarvelService';
 import './style/style.scss';
 
 
-const marvelService = new MarvelService();
-
-
-marvelService.getCharacter(1010338).then((res) => console.log(res.data.results[0].name));
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App/>,
 );
-
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
-
+// 1009150
 
 class MarvelService {
+    // разделение API
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
 
     _apiKey = 'apikey=97f5a52a2a73fdc86ffa7ea3c6417008';
@@ -15,11 +16,30 @@ class MarvelService {
         return res.json();
     };
 
-    // eslint-disable-next-line no-underscore-dangle
-    getAllCharacters = () => this.getResource(`${this._apiBase}characters?${this._apiKey}`);
+    // берем всех персонажей
+    getAllCharacters = async () => {
+        // eslint-disable-next-line no-underscore-dangle
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&${this._apiKey}`);
+        // eslint-disable-next-line no-underscore-dangle
+        return res.data.results.map(this._transformCharacter);
+    };
 
-    // eslint-disable-next-line no-underscore-dangle
-    getCharacter = (id) => this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    // берем одного персонажа
+    getCharacter = async (id) => {
+        // eslint-disable-next-line no-underscore-dangle
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        // eslint-disable-next-line no-underscore-dangle
+        return this._transformCharacter(res.data.results[0]);
+    };
+
+    // eslint-disable-next-line class-methods-use-this
+    _transformCharacter = (char) => ({
+        name: char.name,
+        description: char.description,
+        thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+        homepage: char.urls[0].url,
+        wiki: char.urls[1].url,
+    });
 }
 
 export default MarvelService;
